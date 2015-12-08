cfa4d40ae5798e0103754a6f34be4926f90942a7
MustBeArt <paul@mustbeart.com>
2015-12-07 19:42:46 -0800

    Bring gui-supervisor.py into compliance with PEP8 (mostly)
    One remaining complaint is about unaligned hanging indents, but these
    are indented properly for numbers (like decimal tabs).

--- gui-supervisor.py
+++ gui-supervisor.py
@@ -16,17 +16,13 @@ been steady for a while, we will launch the new program (which might be
 the same as the old program).
 
 2015-05-25 Paul Williamson
-2015-08-24 ptw  Reconstructing lost work: actually launch programs and signal them,
-				and don't use the screen.
-				Added syslog logging.
-2015-08-25 ptw  Adjusted pin number to match as-built configuration.
-
 """
 
 # If we're running on Raspbian Jessie, we can use GPIO without being root!
 # Otherwise, must run as root to use the built-in GPIO package.
 import RPi.GPIO as GPIO
-import sys, time
+import sys
+import time
 import subprocess
 import syslog
 import signal
@@ -35,48 +31,52 @@ syslog.openlog("organelle")
 syslog.syslog(syslog.LOG_INFO, "Organelle GUI supervisor started")
 
 switch_steady_delay = 1.0  # seconds before the switch is considered stable
-proc_exit_delay = 1.0		# seconds to allow the process to exit
+proc_exit_delay = 1.0       # seconds to allow the process to exit
 
 # Pin numbers will follow the Broadcom SoC pin numbering
 GPIO.setmode(GPIO.BCM)
 
 # Mapping of pins onto programs and their command-line arguments
-programs = {  4: ("./gui-play-midi.py", ""),
-             17: ("xterm", "-fullscreen -e ./organelle.py t"),
-             27: ("./gui-play-dark.py", ""),
-             22: ("./autoplay.py", ""),
-              5: ("./autoplay.py", ""),
-              6: ("xterm", "-fullscreen -e ./organelle.py g"),
-             13: ("./gui-play-waterfall.py", ""),
-             19: ("./gui-play-keyboards.py", ""),								# auto-play
-             26: ("./gui-play-waterfall.py", ""),
-             23: ("./gui-play-dark.py", "")
-              }
+programs = {
+         4: ("./gui-play-midi.py", ""),
+        17: ("xterm", "-fullscreen -e ./organelle.py t"),
+        27: ("./gui-play-dark.py", ""),
+        22: ("./autoplay.py", ""),
+         5: ("./autoplay.py", ""),
+         6: ("xterm", "-fullscreen -e ./organelle.py g"),
+        13: ("./gui-play-waterfall.py", ""),
+        19: ("./gui-play-keyboards.py", ""),
+        26: ("./gui-play-waterfall.py", ""),
+        23: ("./gui-play-dark.py", "")
+        }
 
 # Extract the list of GPIO pins from the program mapping.
 pins = programs.keys()
 
+
 # Function that reads all the pins into a dictionary.
 def rotary_switch():
-    return {x : GPIO.input(x) for x in pins}
+    return {x: GPIO.input(x) for x in pins}
+
 
 # Given a dictionary levels containing the pin levels,
 # and hoping that exactly one of them is 0 (because it's a rotary switch),
 # return the pin number of the first one that's 0.
 # If somehow none of the pins are grounded, return None.
 def selected(levels):
-    for pin,val in levels.iteritems():
+    for pin, val in levels.iteritems():
         if val == 0:
             return pin
     return None
 
+
 # Display a prompt in case the screen is unclaimed long enough to matter.
-#def prompt():
-#    sys.stderr.write("\x1b[2J\x1b[10;1H")    # row 10, column 1
-#    print "Select mode using rotary switch"
+# def prompt():
+#     sys.stderr.write("\x1b[2J\x1b[10;1H")    # row 10, column 1
+#     print "Select mode using rotary switch"
 def prompt():
-	# we need not do anything to prompt; the wallpaper is the prompt.
-	pass
+    # we need not do anything to prompt; the wallpaper is the prompt.
+    pass
 
 # Set all pins as inputs with pullup, so we just ground a pin to activate.
 for p in pins:
@@ -100,16 +100,14 @@ while True:
     choice = selected(levels)
     if choice is None:
         continue
-    (prog,arg) = programs[choice]
+    (prog, arg) = programs[choice]
 
-    # dummy launch for testing
-    #print "Here we launch %s %s" % (prog,arg)
     proc = subprocess.Popen([prog]+arg.split())
     if not proc:
         syslog.syslog(syslog.LOG_ERROR, "Failed to launch " + prog + " " + arg)
         continue
     syslog.syslog(syslog.LOG_INFO, "Launched " + prog + " " + arg)
-    
+
     # Program is running. Continue watching the rotary switch for changes.
     while levels == rotary_switch():
         time.sleep(0.100)
@@ -120,13 +118,11 @@ while True:
 #    waitfor = time.time() + proc_exit_delay
 #    while time.time() < waitfor:
 #        if proc.poll():
-#	    syslog.syslog(syslog.LOG_INFO, "Normal exit")
+#       syslog.syslog(syslog.LOG_INFO, "Normal exit")
 #            break
 #        time.sleep(0.100)
 #    if not proc.poll():
 #        # uh oh, program didn't exit as requested. Terminate with prejudice.
 #        syslog.syslog(syslog.LOG_ERR, "Program failed to exit on request!")
 #        proc.kill()
-#        proc.wait()		# if kill() doesn't work, we're hung too.
-
-
+#        proc.wait()        # if kill() doesn't work, we're hung too.
